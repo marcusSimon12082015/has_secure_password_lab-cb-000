7aa72b0075338301ab49ed94ec207665ee69e141
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2018-12-30 12:18:27 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,10 @@ class UsersController < ApplicationController
 
   def new
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name,:password,:password_confirmation)
+  end
 end

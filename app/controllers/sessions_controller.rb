0176367190c24d6115749104c3d342667821261d
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2018-12-30 12:49:41 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
   def create
     @user = User.find_by(name:params[:user][:name])
-    if !params[:password].empty? && !@user.authenticate(params[:user][:password]).nil?
+    if !@user.authenticate(params[:user][:password]).nil?
       session[:user_id] = @user.id
     end
   end

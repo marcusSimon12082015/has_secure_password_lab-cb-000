4a9fcee54129071837c1bbe01f96cbb73336b9c6
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2018-12-30 13:23:28 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
     if params[:user][:password] && !params[:user][:name].empty? &&
       !User.find_by(name:params[:user][:name]).nil?
       @user = User.find_by(name:params[:user][:name])
-      if !@user.authenticate(params[:user][:password])
+      if @user.authenticate(params[:user][:password])
         session[:user_id] = @user.id
       else
         session[:user_id] = nil

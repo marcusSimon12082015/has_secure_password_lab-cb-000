3c74c8463df53376fd207e2e44aa799ea55847e5
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2018-12-30 13:12:13 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,11 @@
 class SessionsController < ApplicationController
   def create
-    byebug
-    @user = User.find_by(name:params[:user][:name])
-    if !@user.authenticate(params[:user][:password]).nil?
-      session[:user_id] = @user.id
+    if params[:user][:password] && !params[:user][:name].empty? && 
+      !User.find_by(name:params[:user][:name]).nil?
+      @user = User.find_by(name:params[:user][:name])
+      if !@user.authenticate(params[:user][:password]).nil?
+        session[:user_id] = @user.id
+      end
     end
   end
 end

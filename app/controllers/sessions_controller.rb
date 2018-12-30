c66bde4d5b840534f889ece1f69f3e16add0613f
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2018-12-30 12:44:50 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class SessionsController < ApplicationController
   def create
+    byebug
     @user = User.find_by(name:params[:user][:name])
     if @user.authenticate(params[:user][:password])
       session[:user_id] = @user.id
